800713789038613dcb3fc803bf29dca533e2600f
pereira valentin <pereira-valentin@outlook.fr>
2019-06-11 17:56:00 +0200

    consoleApp1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            int x = 3;
+            i = i + x;
+            x = i - x;
+            i = i - x;
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+        }
+    }
+}
